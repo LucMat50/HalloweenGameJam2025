5dbdf928f9855c89e9780bda45a1e0872564d20d
LucMat50 <lmaturino5@gmail.com>
2025-10-28 22:40:52 -0700

    Interaction complete => change("Open") needs to be changed

--- halloween-game-jam-2025/Scripts/key_interactable.gd
+++ halloween-game-jam-2025/Scripts/key_interactable.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 #VARIABLES
+#@onready var slots = $Inv_UI/NinePatchRect/GridContainer.get_children()
 var player_in_range:bool = false
+var player_has_key:bool = false
 
+#FUNCTIONS
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("Able to Interact")
@@ -12,6 +15,12 @@ func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_in_range = false
 
-func _process(delta: float):
+func _on_key_key_1() -> void:
+	player_has_key = true
+	print("Player has key!")
+	
+#CHECKS IF PLAYER HAS COLLECTED KEY1
+func _process(_delta: float):
 	if player_in_range and Input.is_action_just_pressed("interact"):
-		pass
+		if player_has_key:
+			print("Open")
