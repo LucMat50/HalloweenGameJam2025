92c68c6c0f22e589e8338d79b53b78bc8c142906
Marco Joson <littlemjoson@gmail.com>
2025-10-31 16:58:09 -0700

    Added fade-in and fade-out transition

--- halloween-game-jam-2025/Scripts/start_menu.gd
+++ halloween-game-jam-2025/Scripts/start_menu.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 
-
+var button_type = null
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+	button_type = "start"
+	$fade_transition.show()
+	$fade_transition/fade_timer.start()
+	$fade_transition/AnimationPlayer.play("fade_in")
+	print("Started")
+	for child in get_children():
+		print("Child of StartMenu:", child.name)
 
 
 func _on_options_pressed() -> void:
@@ -13,3 +19,8 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start" : 
+		get_tree().change_scene_to_file("res://Scenes/World.tscn")
--- halloween-game-jam-2025/Scripts/world.gd
+++ halloween-game-jam-2025/Scripts/world.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready():
+	$Player/Camera2D/CanvasLayer/fade_transition/AnimationPlayer.play("fade_out")
 
 func _on_world_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/bad_end_cutscene.tscn")
