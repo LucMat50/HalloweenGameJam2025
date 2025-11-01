7aef7b95cf9403c40d8d44011ae4465479f0756c
LucMat50 <lmaturino5@gmail.com>
2025-11-01 13:55:27 -0700

    bad ending animations

---
+++ halloween-game-jam-2025/Scripts/bad_end_cutscene.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+var fading_in = false
+
+@onready var label_animation = $Label/AnimationPlayer
+@onready var colorRect_animation = $ColorRect/AnimationPlayer
+@onready var hand_animation = $Hand/AnimationPlayer
+@onready var colorRect2_animation = $ColorRect2/AnimationPlayer
+@onready var label2_animation = $Label2/AnimationPlayer
+
+func _ready() -> void:
+	label_animation.animation_finished.connect(_on_label_animation_finished)
+	colorRect_animation.animation_finished.connect(_on_colorRect_animation_finished)
+	hand_animation.animation_finished.connect(_on_hand_animation_finished)
+	colorRect2_animation.animation_finished.connect(_on_colorRect2_animation_finished)
+	label2_animation.animation_finished.connect(_on_label2_animation_finished)
+	$Label.show()
+	colorRect_animation.play("RESET_black")
+	$ColorRect2.hide()
+	$Label2.hide()
+	$Control/GameOver.hide()
+	$Label/AnimationPlayer.play("fade_in")
+
+
+func _on_label_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		label_animation.play("fade_out")
+	elif anim_name == "fade_out":
+		$Label.hide()
+		colorRect_animation.play("fade_out")
+		
+func _on_colorRect_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_out":
+		hand_animation.play("Grab")
+
+func _on_hand_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Grab":
+		$ColorRect2.show()
+		colorRect2_animation.play("fade_in")
+
+func _on_colorRect2_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		$Control/Butcher.hide()
+		$Hand.hide()
+		$Label2.show()
+		$Control/GameOver.show()
+		label2_animation.play("fade_in")
+
+func _on_label2_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		label2_animation.play("fade_out")
+	elif anim_name == "fade_out":
+		$Label2.hide()
+		colorRect2_animation.play("fade_out")	
