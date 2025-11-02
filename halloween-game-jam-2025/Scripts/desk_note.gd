ac6f5aa38af857ac4474d50428b013dcc7d14eeb
LucMat50 <lmaturino5@gmail.com>
2025-11-01 19:18:49 -0700

    added floor2 desk note

---
+++ halloween-game-jam-2025/Scripts/desk_note.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@export var item: InvItem
+var player = null
+var player_in_range = false
+signal deskNote
+
+func _ready() -> void:
+	$Sprite2D.hide()
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+	
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		print("Enter")
+		player_in_range = true
+		player = body
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_in_range = false
+		player = null
+
+func _process(_delta):
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		if player:
+			deskNote.emit()
+			var success = player.tryCollect(item)  # give item to player
+			if success:
+				queue_free()
