09a6bbc507011119c26056d4ef8699cd86e92b79
isc-bll <isacan0527@gmail.com>
2025-11-01 18:33:24 -0700

    Fixed safe dialog not showing

--- halloween-game-jam-2025/Scripts/closet_safe_object.gd
+++ halloween-game-jam-2025/Scripts/closet_safe_object.gd
@@ -25,3 +25,4 @@ func _on_body_exited(body):
 func _process(_delta):
 	if player_in_range and Input.is_action_just_pressed("interact"):
 		open_closet.show()
+		player.dialog_manager.start_dialog(item.description_timeline)
